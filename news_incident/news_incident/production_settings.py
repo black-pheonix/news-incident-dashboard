5a0ba566634633bc3173f64a893faf9592474805
Vipul Jain <www.vipuljains17031972@gmail.com>
2026-06-17 22:51:44 +0530

    update requirements for production

---
+++ news_incident/news_incident/production_settings.py
@@ -0,0 +1,22 @@
+from .settings import *
+import dj_database_url
+import os
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['*'] 
+
+# Database
+DATABASE_URL = os.environ.get('DATABASE_URL')
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.parse(DATABASE_URL)
+    }
+
+# Static files
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Security
+SECRET_KEY = os.environ.get('SECRET_KEY', SECRET_KEY)
